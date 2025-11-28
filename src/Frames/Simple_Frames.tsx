31e3cf0e210e004f6d5a02d2e833d9c6217aeaa5
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-11-29 01:49:26 +0530

    frames

--- src/Frames/Simple_Frames.tsx
+++ src/Frames/Simple_Frames.tsx
@@ -6,7 +6,7 @@ export function Frame1() {
   const { image1, title, message, name, } = useValues()
   return (
     <div style={{ width: "340px", backgroundImage: `url(${bg1})`, backgroundSize: "cover" }} className="p-4 pt-0 border boder-1">
-      <div style={{ fontSize: "40px", WebkitTextStrokeColor: "skyblue", WebkitTextStrokeWidth: "2px", textShadow: "10px 10px 10px black" }} className="fw-bolder text-warning" >{title}</div>
+      <div style={{ fontSize: "43px", WebkitTextStrokeColor: "#000000", WebkitTextStrokeWidth: "1px" }} className="fw-bolder text-warning" >{title}</div>
       <div className="rounded-circle overflow-hidden mb-2" style={{ width: "200px", height: "200px", margin: "auto", border: "2px ridge skyblue" }}>
         <img src={image1 as string} alt="pic" className="w-100" />
       </div>
@@ -19,13 +19,15 @@ export function Frame1() {
 export function Frame2() {
   const { image1, title, message, name } = useValues()
   return (
-    <div className="border border-warning text-center text-black p-3 pt-0" style={{ width: "340px", backgroundImage: `url(${bg2})`, backgroundSize: "cover" }}>
-      <div className="text-info fw-bolder text-center" style={{ fontSize: "40px", WebkitTextStrokeColor: "pink", WebkitTextStrokeWidth: "2px", textShadow: "10px 10px 10px black" }}>{title}</div>
-      <div className="mb-1 border border-warning overflow-hidden mx-auto bg-black" style={{ width: "210px", height: "260px" }} >
+    <div className="border border-warning text-center text-black p-3 pt-0 position-relative"
+      style={{ width: "340px", backgroundImage: `url(${bg2})`, backgroundSize: "cover" }}>
+      <div className=" position-absolute top-50 bottom-0 end-0 start-0" style={{ background: "linear-gradient(transparent,pink)" }}></div>
+      <div className="text-info fw-bolder text-center position-relative" style={{ fontSize: "40px", textShadow: "0px 0px 3px black" }}>{title}</div>
+      <div className="mb-1 border position-relative border-warning overflow-hidden mx-auto bg-black" style={{ width: "210px", height: "260px" }} >
         <img src={image1 as string} alt="pic" className="w-100" />
       </div>
-      {name && <div className="bg-danger-subtle fs-3 fw-bold edu-hand">{name}</div>}
-      <div className="nunito" style={{ backgroundColor: "#ffffff90" }}>{message}</div>
+      {name && <div className="bg-danger-subtle fs-3 fw-bold edu-hand position-relative">{name}</div>}
+      <div className="nunito position-relative">{message}</div>
     </div>
   )
 }
@@ -69,21 +71,21 @@ export function Frame5() {
   const { image1, title, message, name } = useValues()
   return (
     <div style={{ width: "340px", backgroundColor: "#d0dbe1" }} className="p-3 text-dark position-relative border border-1 border-white">
-      <div className="position-absolute bottom-0" style={{ left: 0,right:0,top:"4px" }}>
-        <div className="h-50 rounded-circle" style={{borderTop:"2px solid #f7f7f7ff"}}></div>
+      <div className="position-absolute bottom-0" style={{ left: 0, right: 0, top: "4px" }}>
+        <div className="h-50 rounded-circle" style={{ borderTop: "2px solid #f7f7f7ff" }}></div>
         <div className="h-50 bg-white rounded-top-circle"></div>
       </div>
-      <div className="fw-bold gochi-hand mb-1" style={{fontSize:"35px"}}>{title}</div>
+      <div className="fw-bold gochi-hand mb-1" style={{ fontSize: "35px" }}>{title}</div>
       <div className="position-relative mx-auto rounded-circle p-1" style={{ width: "220px", height: "220px", background: "linear-gradient(to right,black,blue)" }}>
         <div className="rounded-circle w-100 h-100 overflow-hidden">
-          <span className=" position-absolute fs-1" style={{top:0,left:"-20px"}}>✨</span>
-          <span className=" position-absolute fs-1" style={{top:"50%",right:"-40px"}}>✨</span>
+          <span className=" position-absolute fs-1" style={{ top: 0, left: "-20px" }}>✨</span>
+          <span className=" position-absolute fs-1" style={{ top: "50%", right: "-40px" }}>✨</span>
           <img src={image1 as string} alt="image" className="w-100" />
-          <div className=" position-absolute d-flex justify-content-center w-100 p-1 pb-4 fw-semibold" style={{ background: "linear-gradient(#d0dbe1,white)",left:0,bottom:"-20px"}}>{name}</div>
+          <div className=" position-absolute d-flex justify-content-center w-100 p-1 pb-4 fw-semibold" style={{ background: "linear-gradient(#d0dbe1,white)", left: 0, bottom: "-20px" }}>{name}</div>
         </div>
       </div>
-      <div className="m-auto position-relative w-25 mb-2 mt-3" style={{border:"1px solid #a6a6a6ff"}}></div>
-      <div style={{fontSize:"small"}} className="position-relative nunito fw-medium text-dark">{message}</div>
+      <div className="m-auto position-relative w-25 mb-2 mt-3" style={{ border: "1px solid #a6a6a6ff" }}></div>
+      <div style={{ fontSize: "small" }} className="position-relative nunito fw-medium text-dark">{message}</div>
     </div>
   )
 }
